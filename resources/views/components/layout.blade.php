0b1c7c116593b85752b3b2332ed40b6336eb47cc
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-11 17:59:59 +0600

    Layout file

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+</head>
+
+<body>
+    <nav>
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">About</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
+    </nav>
+
+    {{ $slot }}
+</body>
+
+</html>
