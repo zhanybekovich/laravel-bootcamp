dbf0ccdb61466335800f8b937a609ff8362c1878
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-12 18:24:59 +0600

    Pivot tables

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Arr;
 
 class Job extends Model
@@ -21,5 +22,10 @@ class Job extends Model
     public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
     }
 }
