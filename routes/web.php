f6cc3560862a8ff3c2a89d3fb2bca50ea0312a52
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-12 18:37:25 +0600

    Eager loading

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => Job::all()]);
+    $jobs = Job::with('employer')->get();
+
+    return view('jobs', ['jobs' => $jobs]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
