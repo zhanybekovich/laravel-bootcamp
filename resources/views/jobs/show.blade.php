becc3a627fc66bf4476e92ace95e2b3ae60434e7
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-12 21:00:23 +0600

    Edit, update, delete

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -7,4 +7,8 @@
     <p>
         This job pays {{ $job['salary'] }} per year.
     </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')
         //   ->paginate(2);
@@ -17,10 +18,12 @@ Route::get('/jobs', function () {
     return view('jobs.index', ['jobs' => $jobs]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
@@ -28,6 +31,7 @@ Route::get('/jobs/{id}', function ($id) {
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
 
     request()->validate([
@@ -40,6 +44,45 @@ Route::post('/jobs', function () {
         'salary' => request('salary'),
         'employer_id' => 1,
     ]);
+
+    return redirect('/jobs');
+});
+
+// Show Edit Form
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // authorize (TODO)
+
+    // update and persist
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    $job = Job::findOrFail($id);
+
+    $job->delete();
 
     return redirect('/jobs');
 });
