dc6a5ad11c3e3fc14c9c484fc774989da402b6ca
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-12 16:18:15 +0600

    Two key relationship

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,16 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Arr;
 
 class Job extends Model
 {
     use HasFactory;
-    
+
     protected $table = 'job_listings';
 
     protected $fillable = [
         'title',
         'salary'
     ];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
