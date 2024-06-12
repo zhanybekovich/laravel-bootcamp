4270e30a1e5d540afb6657bedee933ea6ff43062
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-12 15:32:14 +0600

    Meet eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Directory',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$150,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$1250,000'
-            ],
-        ];
-    }
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array
-    {
-        $job =  Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = [
+        'title',
+        'salary'
+    ];
 }
